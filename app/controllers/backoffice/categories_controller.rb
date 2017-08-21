4893e1ef022a819e79d80fb2081a59486ca4258d
rbricardo <rb.ricardo01@gmail.com>
2017-08-21 15:23:10 -0300

    First commit

---
+++ app/controllers/backoffice/categories_controller.rb
@@ -0,0 +1,52 @@
+class Backoffice::CategoriesController < BackofficeController
+  
+  before_action :set_category, only: [:edit, :update]
+   
+
+  def index
+  	@categories = Category.all
+  end
+
+  def new
+  	@category = Category.new
+  end
+
+  def create
+  	@category = Category.new(params_category)
+
+    if @category.save
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi salva com sucesso! "
+    else
+      render :new   
+    end
+
+  end
+
+  def edit
+  	
+  end
+
+  def update
+
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "A categoria (#{@category.description}) foi editada com sucesso! "
+    else
+      render :edit  
+    end
+  end
+
+  private
+
+    def params_category
+
+      params.require(:category).permit(:description)
+      
+    end
+
+    def set_category
+
+          @category = Category.find(params[:id])
+      
+    end
+
+end
---
+++ app/controllers/backoffice_controller.rb
@@ -0,0 +1,8 @@
+
+class BackofficeController < ApplicationController
+	
+	before_action :authenticate_admin!
+
+    layout "backoffice"
+
+end
